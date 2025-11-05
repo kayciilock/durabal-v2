08027f4de81b036719600e655957130ee3a39e86
Kay - CiiLOCK Engineering <kineshak@ciilock.com>
2025-11-05 14:54:11 +1100

    new sust

---
+++ js/query.tabs.js
@@ -0,0 +1,28 @@
+function designStudioEnquire() {
+    sessionStorage.setItem("pageLocation", "designStudio");
+    console.log("pageLocation set to:", sessionStorage.getItem("pageLocation"));
+}
+
+function inspoGalleryEnquire() {
+    sessionStorage.setItem("pageLocation", "inspoGallery");
+}
+
+function energyEfficiencyEnquire() {
+    sessionStorage.setItem("pageLocation", "energyEfficiency");
+}
+
+function designEventsEnquire() {
+    sessionStorage.setItem("pageLocation", "designEvents");
+}
+
+function retrofitEnquire() {
+    sessionStorage.setItem("pageLocation", "retrofit");
+}
+
+function newProjectEnquire() {
+    sessionStorage.setItem("pageLocation", "newProject");
+}
+
+function newProjectEnquire() {
+    sessionStorage.setItem("pageLocation", "newProject");
+}
